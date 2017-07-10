b47f257b16baf3d40719a08ad9787401ba4f16e4
Vlad <unix.vlad@yandex.ru>
2017-07-10 21:12:08 +0300

    create test

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,6 +1,17 @@
+import pytest
+
+from book_store import book, category
 
 
+@pytest.fixture(scope="module")
+def cat():
+    return category.Category("Awesome books")
 
+@pytest.fixture(scope="module")
+def item():
+    return book.Book("Some title", "Some author", 50)
 
-def test_category_add_book():
-    pass
+def test_add_book(cat, item):
+    cat.add_book(item)
+    assert item.id in cat.items
+
