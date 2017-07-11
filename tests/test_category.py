2e203d6256de8ebff9e60a7407685b7709591858
Vlad <unix.vlad@yandex.ru>
2017-07-11 20:20:25 +0300

    add some tests

--- tests/test_category.py
+++ tests/test_category.py
@@ -3,15 +3,24 @@ import pytest
 from book_store import book, category
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def cat():
     return category.Category("Awesome books")
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def item():
     return book.Book("Some title", "Some author", 50)
 
-def test_add_book(cat, item):
+def test_add_book(cat, item):     
     cat.add_book(item)
-    assert item.id in cat.items
+    assert item.id in cat.items 
+    with pytest.raises(book.BookAlreadyExist):
+        cat.add_book(item)
+
+def test_find_by_title(cat, item):
+    cat.add_book(item)
+    found_book = cat.find_by_title(item.title)
+    assert found_book is item
+    with pytest.raises(book.BookDoesNotExist):
+        found_book = cat.find_by_title("wrong title")
 
