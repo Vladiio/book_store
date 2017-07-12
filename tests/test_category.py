81e9dccd32d479d01fac46ff40cb22c968bd6f51
Vlad <unix.vlad@yandex.ru>
2017-07-12 17:57:11 +0300

    group tests in class

--- tests/test_category.py
+++ tests/test_category.py
@@ -7,20 +7,28 @@ from book_store import book, category
 def cat():
     return category.Category("Awesome books")
 
+
 @pytest.fixture(scope="function")
 def item():
     return book.Book("Some title", "Some author", 50)
 
-def test_add_book(cat, item):     
-    cat.add_book(item)
-    assert item.id in cat.items 
-    with pytest.raises(book.BookAlreadyExist):
-        cat.add_book(item)
 
-def test_find_by_title(cat, item):
-    cat.add_book(item)
-    found_book = cat.find_by_title(item.title)
-    assert found_book is item
-    with pytest.raises(book.BookDoesNotExist):
-        found_book = cat.find_by_title("wrong title")
+class TestCategory:
+    def test_add_book(self, cat, item):
+        cat.add_book(item)
+        assert item.id in cat.items 
+
+    def test_add_book_identical(self, cat, item):     
+        cat.add_book(item)
+        with pytest.raises(book.BookAlreadyExist):
+            cat.add_book(item)
+
+    def test_find_by_title(self, cat, item):
+        cat.add_book(item)
+        found_book = cat.find_by_title(item.title)
+        assert found_book is item
+
+    def test_find_by_title_does_not_exist(self, cat, item):
+        with pytest.raises(book.BookDoesNotExist):
+            cat.find_by_title("wrong title")
 
