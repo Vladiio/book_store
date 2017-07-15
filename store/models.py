0ffecf788d199af28b898e580ab0c53aad65ec7b
Vlad <vladpochta5@gmail.com>
2017-07-15 21:16:23 +0300

    provide relationship for orm

--- store/models.py
+++ store/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, \
+         String, ForeignKey
+from sqlalchemy.orm import relationship
 
 from database.conf import Base, session
 
@@ -10,6 +12,11 @@ class Book(Base):
     title = Column(String)
     author = Column(String)
     price = Column(Integer)
+    category_id = Column(Integer,
+                         ForeignKey("categories.id"))
+
+    category = relationship("Category",
+                            back_populates="books")
 
     def __str__(self):
         return "{0.id}, {0.title}, " \
@@ -19,41 +26,12 @@ class Book(Base):
         session.add(self)
 
 
-class Category:
-    """Represent category of books.
+class Category(Base):
+    __tablename__ = "categories"
     
-    """
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
     
-    def __init__(self, name):
-        self.name = name
-        self.items = {}
-
-    def display_books(self):
-        """Print all books in the category."""
-        for item in self.items.values():
-            print(item)
-    
-    def add_book(self, item):
-        """Add book to the category."""
-        if isinstance(item, book.Book):
-            if item.id not in self.items:
-                self.items[item.id] = item
-            else:
-                raise book.BookAlreadyExist(id=item.id)
-
-    def find_by_title(self, title):
-        """Return a book with specified title."""
-        for item in self.items.values():
-            if item.title == title:
-                return item
-        else:
-            raise book.BookDoesNotExist(title=title)
-    
-    def find_by_id(self, id):
-        try:
-            item = self.items[id] 
-        except KeyError:
-            raise book.BookDoesNotExist(id=id)
-        else:
-            return item
+    books = relationship("Book", order_by=Book.id,
+                         back_populates="category")
 
