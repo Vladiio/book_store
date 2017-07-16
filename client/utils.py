d706b864a0cca10c4462d10bfd66eecef91e4978
Vlad <vladpochta5@gmail.com>
2017-07-16 18:36:26 +0300

    create functions for valid input

--- client/utils.py
+++ client/utils.py
@@ -1,11 +1,28 @@
 
 
-def get_int(msg):
+def get_int(msg, default=None):
+    msg = msg if default is None else msg + f" ({default})"
     while True:
-        response = input(msg)
+        response = input(msg + ": ")
+        if response == "":
+            return default
         try:
             number = int(response)
         except ValueError:
             print("Only numbers allowed")
         else:
             return number
+
+
+def get_string(msg, default=None):
+    msg = msg if default is None else msg + f" ({default})"
+    while True:
+        response = input(msg + ": ")
+        if response == "":
+            return default
+        elif len(response) <= 3:
+            print("Category length must be "
+                  "longer than 3 characters")
+        else:
+            return response
+
