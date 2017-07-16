11305932a0ce0afcb065f75d8ba7d3bfdd659677
Vlad <vladpochta5@gmail.com>
2017-07-16 13:42:00 +0300

    provide template for menu

--- client/menu.py
+++ client/menu.py
@@ -4,17 +4,18 @@ from client import commands
 class Menu:
     def __init__(self):
         self.commands = {
-            "/show": commands.display_books,
+            "/show_cats": commands.display_categories,
+            "/show_books": commands.display_books,
             "/q": commands.quit,
         }
+        self.template = "\nShow all categories: {0}\n" \
+                        "Show books from category" \
+                        "Quit: {1}\n"
 
     def run(self):
         while True:
-            print("""
-\tShow books:\t /show
-\tQuit\t\t /q
-                  """
-                  )
+            args = self.commands.keys()
+            print(self.template.format(*args))
             response = input("Your choice: ")
             try:
                 action = self.commands[response]
