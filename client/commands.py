4d6c2f89b2f24c06bba798f4660f0ed7a66aabf4
Vlad <vladpochta5@gmail.com>
2017-07-16 13:40:57 +0300

    add new command

--- client/commands.py
+++ client/commands.py
@@ -1,14 +1,25 @@
 import sys
 
-from store import views
+from store.views import main_view
+from store.models import Category, Book
+from client.utils import get_int
+
+
+def display_categories():
+    cats = main_view.get_all()
+    print("ID\tNAME\tBOOKS")
+    for cat in cats:
+        print(cat)
 
 
 def display_books():
-    books = views.book_view()
+    cat_id = get_int("Enter category id please: ")
+    books = main_view.get_books(cat_id)
     for book in books:
         print(book)
-    
+
 
 def quit():
     print("Buy buy")
     raise sys.exit(1)
+
