11e3db19ed0d13f072cbd29a8302ed49dbf8a9c9
Vlad <vladpochta5@gmail.com>
2017-07-16 18:38:06 +0300

    extand base methods

--- store/views.py
+++ store/views.py
@@ -6,12 +6,28 @@ class MainView:
     def __init__(self):
         self.session = session
         
-    def get_all(self, model=Category):
-        return self.session.query(model).all() 
+    def get_categories(self, cat_name):
+        if cat_name == "all":
+            result = self.session.query(Category).all() 
+        else:
+            result = self.session.query(Category).\
+                        filter(Category.name==cat_name)
+        return result
 
     def get_books(self, cat_id):
-        return self.session.query(Book).\
-                filter(Category.id==cat_id).all()
+        if cat_id == 0:
+            result = self.session.query(Book).all()
+        else:
+            result = self.session.query(Book).\
+                    filter(Category.id==cat_id).all()
+        return result   
+
+    def create_book(self, category, 
+                    title, author, price):
+        pass
+    
+    def create_category(self, cat_name):
+        pass
 
 
 main_view = MainView()
