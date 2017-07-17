1b1ccf033a5010e7de9bed0aa265c73923a91863
Vlad <unix.vlad@yandex.ru>
2017-07-17 17:36:14 +0300

    add command create category

--- client/commands.py
+++ client/commands.py
@@ -1,6 +1,6 @@
 import sys
 
-from store.views import main_view
+from store.views import category_view, book_view
 from store.models import Category, Book
 from store import exc
 from client.utils import get_int, get_string, \
@@ -11,20 +11,20 @@ from database.conf import session
 def display_categories(request):
     cat_name = get_string("Enter category name please",
                           default="all")
-    print("\tID\tNAME\tBOOKS")
     if cat_name == "all":
-        cats = main_view.get_category_list()
+        print("\tID\tNAME\tBOOKS")
+        cats = category_view.get_all()
         for cat in cats:
             print(cat)
     else:
-        cat = main_view.get_category(cat_name)
+        cat = category_view.get(cat_name)
         print(cat)
 
 
 def display_books(request):
     cat_id = get_int("Enter category id please")
+    books = category_view.get(cat_id)
     print("\tID\tTITLE\tAUTHOR\tPRICE") 
-    books = main_view.get_book_list(cat_id)
     for book in books:
          print(book)
 
@@ -32,12 +32,22 @@ def display_books(request):
 def add_book(request):
     book_info = get_book_info()
     try:
-        main_view.create_book(*book_info)
-    except exc.ObjectDoesNotExist as err:
+        book_view.create(*book_info)
+    except exc.CategoryDoesNotExist as err:
         print(f"Error: category {err.name} does not exist")
     else:
         request.changes = True
         print("Book created")
+
+
+def add_category(request):
+    cat_name = get_string("Enter category name")
+    try:
+        category_view.create(cat_name)
+    except exc.CategoryAlreadyExist as err:
+        print(f"Error: category {err.name} exists")
+    else:
+        print("Category created")
 
 
 def quit(request):
--- client/menu.py
+++ client/menu.py
@@ -7,13 +7,15 @@ class Menu:
         self.commands = {
             "/show_cats": commands.display_categories,
             "/show_books": commands.display_books,
+            "/add_category": commands.add_category,
             "/add_book": commands.add_book,
             "/q": commands.quit,
         }
         self.template = "\n\tShow categories: {0:<10}" \
                         "\n\tShow books:      {1:<10}" \
-                        "\n\tAdd new book:    {2:<10}" \
-                        "\n\tQuit:            {3:<10}\n"
+                        "\n\tAdd new cat:     {2:<10}" \
+                        "\n\tAdd new book:    {3:<10}" \
+                        "\n\tQuit:            {4:<10}\n"
 
     def run(self):
         while True:
