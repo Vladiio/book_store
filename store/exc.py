5ce63800665ea416ba0cf31297ecf44b62a01992
Vlad <unix.vlad@yandex.ru>
2017-07-17 17:37:11 +0300

    add few exceptions

--- store/exc.py
+++ store/exc.py
@@ -1,12 +1,23 @@
 
 
 class StoreException(Exception):
-    pass
-
-
-class ObjectDoesNotExist(StoreException):
     
     def __init__(self, name, *args, **kwargs):
         self.name = name
         super().__init__(*args, **kwargs)
 
+
+class CategoryDoesNotExist(StoreException):
+    pass
+
+
+class BookDoesNotExist(StoreException):
+    pass
+
+
+class CategoryAlreadyExist(StoreException):
+    pass
+
+
+class BookAlreadyExist(StoreException):
+    pass
