3e9fb2e4c4e5abd2c25e028cdfbb2af641a09ea1
Vlad <unix.vlad@yandex.ru>
2017-07-17 17:37:58 +0300

    create abstract class for views

--- store/views.py
+++ store/views.py
@@ -1,38 +1,70 @@
+from abc import ABCMeta, abstractmethod
+
+from sqlalchemy.exc import IntegrityError
+
 from store.models import Book, Category
 from store import exc
 from database.conf import session
 
 
-class MainView:
-        
-    def get_category_list(self):
-        return Category.objects().all()
 
-    def get_category(self, cat_name):
-        return Category.objects().filter(
-                      Category.name==cat_name).first()
-        
-    def get_book_list(self, cat_id):
-        return Book.objects().filter(
-                    Book.category_id==cat_id).all()
+class BaseView(metaclass=ABCMeta):
 
+    def __init__(self, model):
+        self.model = model
+
+    def get_all(self):
+        return self.model.objects().all()
+
+    @abstractmethod
+    def get(self, title):
+        pass        
+
+    @abstractmethod
+    def create(self):
+        pass
     
-    def get_book(self, book_title):
-        return Book.objects().filter(
-                    Book.title==book_title).one()
 
-    def create_book(self, cat_name, 
-                    title, author, price):
-        category = self.get_category(cat_name)
-        if not category:
-            raise exc.ObjectDoesNotExist(cat_name)
+class CategoryView(BaseView):
+
+    def __init__(self):
+        super().__init__(Category)
+        
+    def get(self, title):
+        return self.model.objects().filter(
+                      self.model.name==title).one()
+        
+    def fetch_books(self, cat_id):
+        return self.model.objects().books.all()
+         
+    def create(self, cat_name):
+        cat = self.model(name=cat_name)
+        try:
+            cat.save()
+        except IntegrityError:
+            raise exc.CategoryAlreadyExist(cat_name)
+
+
+category_view = CategoryView()
+
+
+class BookView(BaseView):
+
+    def __init__(self):
+        super().__init__(Book)
+
+    def get(self, title):
+        return self.model.objects().filter(
+                    self.model.title==title).one()
+    
+    def create(self, cat_name, 
+               title, author, price):
+        category = category_view.get(cat_name)
+        
         book = Book(title=title, author=author,
                     category_id=category.id, price=price)
         book.save()
-         
-    def create_category(self, cat_name):
-        pass
 
 
-main_view = MainView()
+book_view = BookView()
 
