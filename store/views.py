a62e62d3b76ca8db24e38c3752c57e405e412359
Vlad <unix.vlad@yandex.ru>
2017-07-18 15:19:47 +0300

    rm boilerplate code

--- store/views.py
+++ store/views.py
@@ -1,5 +1,3 @@
-from abc import ABCMeta, abstractmethod
-
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import NoResultFound 
 
@@ -9,7 +7,7 @@ from database.conf import session
 
 
 
-class BaseView(metaclass=ABCMeta):
+class BaseView:
 
     def __init__(self, model):
         self.model = model
@@ -17,69 +15,30 @@ class BaseView(metaclass=ABCMeta):
     def get_all(self):
         return self.model.objects().all()
 
-    @abstractmethod
-    def get(self, title):
-        pass        
-
-    @abstractmethod
-    def create(self):
-        pass
-    
-
-class CategoryView(BaseView):
-
-    def __init__(self):
-        super().__init__(Category)
-        
     def get(self, title):
         try:
-            category =  self.model.objects().filter(
-                             self.model.name==title).one()
+            obj =  self.model.objects().filter(
+                             self.model.title==title).one()
         except NoResultFound:
-            raise exc.CategoryDoesNotExist(title)
+            raise exc.ObjectDoesNotExist(title)
         else:
-            return category
-        
-    def create(self, cat_name):
-        cat = self.model(name=cat_name)
+            return obj
+    
+    def create(self, **kwargs):
         try:
-            cat.save()
+            obj = self.model(**kwargs)
+            obj.save()
         except IntegrityError:
-            raise exc.CategoryAlreadyExist(cat_name)
-
-
-category_view = CategoryView()
+            raise exc.ObjectAlreadyExist(kwargs.get("title"))
 
 
 class BookView(BaseView):
 
-    def __init__(self):
-        super().__init__(Book)
-
-    def get(self, title):
-        try:
-            book = self.model.objects().filter(
-                        self.model.title==title).one()
-        except NoResultFound:
-            raise exc.BookDoesNotExist(title)
-        else:
-            return book
-
-    def fetch(self, cat_id):
+    def get_all(self, cat_id):
         return self.model.objects().filter(
                     self.model.category_id==cat_id).all()
 
-    def create(self, cat_name, 
-               title, author, price):
-        try:
-            category = category_view.get(cat_name)
-        except IntegrityError:
-            raise exc.BookAlreadyExist(title)
-        else:
-            book = Book(title=title, author=author,
-                        category_id=category.id, price=price)
-            book.save()
 
-
-book_view = BookView()
+category_view = BaseView(Category)
+book_view = BookView(Book)
 
